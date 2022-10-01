b721a415503e3afe7d830c5d7d73ba03dc9e4225
luxxyanjaymabar <luxxyxyzvfx@gmail.com>
2022-10-02 03:36:27 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -549,7 +549,7 @@ const listMessage = {
 }
 handler.help = ['menu', 'help', '?']
 handler.tags = ['main']
-handler.command = /^(menu|help|\?)$/i
+handler.command = /^(m|help|\?)$/i
 
 handler.register = false
 handler.exp = 3
